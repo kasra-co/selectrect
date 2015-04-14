2fa060afd347cd37f03b9119659913ac1efabdf0
Dan Ross <ross9885@gmail.com>
2015-04-14 09:56:00 +0300

    Compensate for parent component offset

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,13 +5,17 @@ let SelectRect = require( "../.." );
 let ExampleComponent = require( "../.." ).ExampleComponent;
 
 let demo = (
-	<SelectRect
-		backgroundSrc={ "/images/selectrect.png" }
-		initialSelection={[ 200, 100, 400, 200 ]}
-		scale={ 1 / 3 }
-		aspectRatio={ 2 / 1 }
-		updateSelection={ function( x0, y0, x1, y1 ) { console.log( x0, y0, x1, y1 ); }}
-	/>
+	<div>
+		<h2>Selection Rectangle Demo</h2>
+		<p>Coordinates are in the console.</p>
+		<SelectRect
+			backgroundSrc={ "/images/selectrect.png" }
+			initialSelection={[ 200, 100, 400, 200 ]}
+			scale={ 1 / 3 }
+			aspectRatio={ 2 / 1 }
+			updateSelection={ function( x0, y0, x1, y1 ) { console.log( x0, y0, x1, y1 ); }}
+		/>
+	</div>
 );
 
 React.render( demo, document.body );
