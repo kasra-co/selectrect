58ff7b4ba3445967b0cb408c103afff9de912d14
Dan Ross <ross9885@gmail.com>
2015-04-16 11:49:50 +0300

    Toggle coordinate trace

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,23 +4,46 @@ let SelectRect = require( "../.." );
 // Require index.js from the root of the project. That is where our module"s interface is specified.
 let ExampleComponent = require( "../.." ).ExampleComponent;
 
-let demo = (
-	<div>
-		<h2>Selection Rectangle Demo</h2>
-		<p>Coordinates are in the console.</p>
-		<SelectRect
-			backgroundSrc={ "/images/selectrect.png" }
-			initialSelection={[ 200, 100, 400, 200 ]}
-			scale={ 1 / 3 }
-			minSize={[ 400, 200 ]}
-			aspectRatio={ 2 / 1 }
-			updateSelection={
-				function( x0, y0, x1, y1 ) {
-					//console.log( x0, y0, x1, y1 );
-				}
-			}
-		/>
-	</div>
-);
+window.tracing = false;
 
-React.render( demo, document.body );
+let Demo = React.createClass({
+	render() {
+		return (
+			<div>
+				<h2>Selection Rectangle Demo</h2>
+				{ this.state.tracing? <p>Coordinates are in the console.</p>: null }
+				<button onClick={ this.toggle }>
+					{ this.state.tracing? "stop ": "" }trace
+				</button><br/>
+				<SelectRect
+					backgroundSrc={ "/images/selectrect.png" }
+					initialSelection={[ 200, 100, 400, 200 ]}
+					scale={ 1 / 3 }
+					minSize={[ 400, 200 ]}
+					aspectRatio={ 2 / 1 }
+					updateSelection={ this.trace }
+				/>
+			</div>
+		);
+	},
+
+	getInitialState() {
+		return {
+			tracing: false
+		};
+	},
+
+	toggle() {
+		this.setState({
+			tracing: !this.state.tracing
+		});
+	},
+
+	trace( x0, y0, x1, y1 ) {
+		if( this.state.tracing ) {
+			console.log( x0, y0, x1, y1 );
+		}
+	}
+});
+
+React.render( <Demo/>, document.body );
